57aeb8235e52473efdd8886563915af36972b0c1
Volodymyr Bezshtanko <v.v.bezshtanko@gmail.com>
2026-05-11 01:06:45 +0300

    rewrite db | pt. 1 | use diffs

--- ReadStat/Data/Database.cs
+++ ReadStat/Data/Database.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Data.Common;
 using System.IO;
 using System.Threading.Tasks;
 using Dapper;
@@ -21,24 +22,49 @@ public static class Database
 
         using var conn = GetConnection();
         conn.Execute(@"
-            CREATE TABLE IF NOT EXISTS Books (
-                Id INTEGER PRIMARY KEY AUTOINCREMENT,
-                Title TEXT NOT NULL,
-                PagesTotal INTEGER,
-                PagesRead INTEGER,
-                CoverId TEXT,
-                Completed INTEGER,
-                Rating INTEGER,
-                CreatedAt TEXT
-            );");
-    }
+            CREATE TABLE IF NOT EXISTS books (
+                book_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                book_title TEXT NOT NULL,
+                book_pages_total INTEGER NOT NULL,
+                book_cover_id TEXT,
+                book_ts DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP 
+            );
 
-    public static List<Book> GetAllBooks()
-    {
-        using var conn = GetConnection();
-        conn.Open();
-        var items = conn.Query<Book>("SELECT * FROM Books ORDER BY CreatedAt DESC").AsList();
-        return items;
+            CREATE TABLE IF NOT EXISTS book_sessions (
+                book_session_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                book_id INTEGER NOT NULL,
+                book_session_pages_read INTEGER NOT NULL,
+                book_session_ts DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                FOREIGN KEY (book_id) REFERENCES books(book_id)
+            );
+
+            CREATE TABLE IF NOT EXISTS book_reviews (
+                book_review_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                book_id INTEGER NOT NULL,
+                book_review_rating INTEGER NOT NULL,
+                book_review_ts DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                FOREIGN KEY (book_id) REFERENCES books(book_id)
+            );
+
+            CREATE VIEW IF NOT EXISTS completed_books AS
+            SELECT * FROM books 
+            WHERE book_pages_total <= 
+                (SELECT sum(book_session_pages_read) FROM book_sessions WHERE book_id = books.book_id);
+
+            CREATE VIEW IF NOT EXISTS unfinished_books AS
+            SELECT b.book_id,
+                   b.book_title,
+                   b.book_pages_total,
+                   b.book_cover_id,
+                   b.book_ts,
+                   coalesce(bs.pages_read, 0) as pages_read
+            FROM books b LEFT JOIN (
+                SELECT bs.book_id as bs_book_id, SUM(bs.book_session_pages_read) as pages_read
+                FROM book_sessions bs
+                GROUP BY bs.book_id
+            ) bs ON b.book_id = bs.bs_book_id
+            WHERE book_pages_total > pages_read or pages_read is null;
+");
     }
 
     public static async Task<List<Book>> ListCompletedBooksAsync()
@@ -46,7 +72,7 @@ public static class Database
         await using var conn = GetConnection();
         await conn.OpenAsync();
         var items = await conn
-            .QueryAsync<Book>("SELECT * FROM Books WHERE Completed=1 ORDER BY CreatedAt DESC");
+            .QueryAsync<Book>("SELECT * FROM completed_books ORDER BY book_ts DESC");
         return items.AsList();
     }
     
@@ -55,7 +81,7 @@ public static class Database
         await using var conn = GetConnection();
         await conn.OpenAsync();
         var items = await conn
-            .QueryAsync<Book>("SELECT * FROM Books WHERE Completed=0 ORDER BY CreatedAt DESC");
+            .QueryAsync<Book>("SELECT * FROM unfinished_books ORDER BY book_ts DESC");
         return items.AsList();
     }
 
@@ -64,7 +90,7 @@ public static class Database
         await using var conn = GetConnection();
         await conn.OpenAsync();
         var count = await conn
-            .ExecuteScalarAsync<int>("SELECT COUNT(*) FROM Books WHERE Completed=1");
+            .ExecuteScalarAsync<int>("SELECT COUNT(*) FROM completed_books");
         return count;
     }
 
@@ -73,32 +99,71 @@ public static class Database
         await using var conn = GetConnection();
         await conn.OpenAsync();
         var totalPages = await conn
-            .ExecuteScalarAsync<int>("SELECT sum(PagesRead) FROM Books");
+            .ExecuteScalarAsync<int>("SELECT sum(book_session_pages_read) FROM book_sessions");
         return totalPages;
     }
 
-    public static Book? GetBook(int id)
+
+    public static async Task<int> AddOrUpdate(Book b)
     {
-        using var conn = GetConnection();
+        await using var conn = GetConnection();
         conn.Open();
-        return conn.QuerySingleOrDefault<Book>("SELECT * FROM Books WHERE Id = @Id", new { Id = id });
+        var tr = await conn.BeginTransactionAsync();
+        var bookId = await UpsertBookAsync(conn, tr, b);
+        await UpdateProgressAsync(conn, tr, new BookProgresUpdateParams(bookId, b.PagesTotal), b.PagesRead);
+        await tr.CommitAsync();
+        return bookId;
     }
 
-    public static int AddOrUpdate(Book b)
+    record struct BookProgresUpdateParams(int bookId, int maxPages);
+    private static async Task UpdateProgressAsync(SqliteConnection conn, DbTransaction tr, BookProgresUpdateParams book,
+        int pagesRead)
     {
-        using var conn = GetConnection();
-        conn.Open();
-        if (b.Id == 0)
+        int currentTotal = await conn.ExecuteScalarAsync<int?>(
+            "SELECT sum(book_session_pages_read) FROM book_sessions WHERE book_id = @BookId",
+            new 
+            {
+                @BookId = book.bookId,
+            }, 
+            transaction: tr) ?? 0;
+
+        var readAdjusted = Math.Max(Math.Min(pagesRead, book.maxPages), 0);
+        var diff = readAdjusted - currentTotal;
+        if (diff == 0) { return; }
+
+        await conn.ExecuteAsync(
+            "INSERT INTO book_sessions(book_id, book_session_pages_read) VALUES (@BookId, @Diff)",
+            new
+            {
+                @BookId = book.bookId,
+                @Diff = diff
+            }, tr);
+    }
+
+    private static async Task<int> UpsertBookAsync(SqliteConnection conn, DbTransaction tr, Book b)
+    {
+        if (b.BookId == 0)
         {
-            var sql = "INSERT INTO Books (Title, PagesTotal, PagesRead, CoverId, Completed, Rating, CreatedAt) VALUES (@Title,@PagesTotal,@PagesRead,@CoverId,@Completed,@Rating,@CreatedAt); SELECT last_insert_rowid();";
-            var id = conn.ExecuteScalar<long>(sql, b);
+            var sql = "INSERT INTO Books (book_title, book_pages_total, book_cover_id) VALUES (@Title,@PagesTotal,@CoverId); SELECT last_insert_rowid();";
+            var id = await conn.ExecuteScalarAsync<long>(sql, new
+            {
+                Title = b.Title,
+                PagesTotal = b.PagesTotal,
+                CoverId = b.CoverId
+            }, tr);
             return (int)id;
         }
         else
         {
-            var sql = "UPDATE Books SET Title=@Title, PagesTotal=@PagesTotal, PagesRead=@PagesRead, CoverId=@CoverId, Completed=@Completed, Rating=@Rating WHERE Id=@Id";
-            conn.Execute(sql, b);
-            return b.Id;
+            var sql = "UPDATE Books SET book_title=@Title, book_pages_total=@PagesTotal, book_cover_id=@CoverId WHERE book_id=@Id";
+            await conn.ExecuteAsync(sql, new
+            {
+                Title = b.Title,
+                PagesTotal = b.PagesTotal,
+                CoverId = b.CoverId,
+                Id = b.BookId
+            }, tr);
+            return b.BookId;
         }
     }
 
@@ -106,6 +171,6 @@ public static class Database
     {
         using var conn = GetConnection();
         conn.Open();
-        conn.Execute("DELETE FROM Books WHERE Id=@Id", new { Id = id });
+        conn.Execute("DELETE FROM books WHERE book_id=@Id", new { Id = id });
     }
 }
